9788629fa823f83eca8df9c1930393f0addd37fe
♿ PoomBoom <gameru2003@gmail.com>
2025-03-19 12:14:16 +0200

    Add project files.

---
+++ Qualification Work/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qualification_Work.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
