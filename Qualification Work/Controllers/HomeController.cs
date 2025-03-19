988df42b51216b6a12419ee9727fb85cfea66655
♿ PoomBoom <gameru2003@gmail.com>
2025-03-19 13:47:12 +0200

    Added Views

--- Qualification Work/Controllers/HomeController.cs
+++ Qualification Work/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Qualification_Work.Models;
 
 namespace Qualification_Work.Controllers;
 
@@ -16,5 +14,28 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+    [HttpGet]
+    public IActionResult Join()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Join(string courseCode)
+    {
+        return RedirectToAction("Index");
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(string courseName)
+    {
+        
+        return RedirectToAction("Index");
     }
 }
